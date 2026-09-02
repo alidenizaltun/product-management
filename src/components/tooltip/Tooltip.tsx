4eaf74dcb5cc78169985aa78a3b5a385680c7fe8
Ali Deniz ALTUN <alidenizaltun66@gmail.com>
2026-09-02 11:43:15 +0300

    Make Tippy the canonical Tooltip implementation (Faz 6)
    B2B'nin Tooltip'i @tippyjs/react kullaniyordu (trigger'i sarar, id
    gerektirmez), PM'ninki reactstrap'in UncontrolledTooltip'ini (target id +
    placement) kullaniyordu - gercek bir mimari catallanmaydi. Tippy kanonik
    secildi; PM'de gercek (routed) hicbir tuketicisi yoktu, sadece olu demo
    sayfalarinda kullaniliyordu (bir onceki commit'te silindi). @tippyjs/react
    ve tippy.js bagimliliklari B2B ile ayni surumde eklendi.

--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -1,44 +1,30 @@
 import React from "react";
-import { UncontrolledTooltip } from "reactstrap";
+import Tippy from "@tippyjs/react";
 import Icon from "@/components/icon/Icon";
 import type { TaggableProps } from "@/components/component.types";
 
-type TooltipDirection = React.ComponentProps<typeof UncontrolledTooltip>["placement"];
-
 type TooltipComponentProps = TaggableProps & {
   iconClass?: string;
-  /** Icon koşulsuz render edildiği için zorunlu. */
   icon: string;
-  /** UncontrolledTooltip hedefi; benzersiz olmalı. */
-  id: string;
-  direction?: TooltipDirection;
-  text?: React.ReactNode;
+  id?: string;
+  text: string;
   containerClassName?: string;
 };
 
 const TooltipComponent = ({
   iconClass,
   icon,
-  id,
-  direction,
   text,
   containerClassName,
-  ...props
+  tag: Tag,
 }: TooltipComponentProps) => {
+  const content = <Icon className={iconClass ?? ""} name={icon} />;
+
   return (
-    <React.Fragment>
-      {props.tag ? (
-        <props.tag className={containerClassName} id={id}>
-          {" "}
-          <Icon className={`${iconClass ? iconClass : ""}`} name={icon}></Icon>
-        </props.tag>
-      ) : (
-        <Icon className={`${iconClass ? iconClass : ""}`} name={icon} id={id}></Icon>
-      )}
-      <UncontrolledTooltip autohide={false} placement={direction} target={id}>
-        {text}
-      </UncontrolledTooltip>
-    </React.Fragment>
+    <Tippy content={text} placement="top">
+      {Tag ? <Tag className={containerClassName}>{content}</Tag> : content}
+    </Tippy>
   );
 };
+
 export default TooltipComponent;

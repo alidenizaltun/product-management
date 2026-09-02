a74e41f0703d4869f775003c42183a07d04d9c27
Ali Deniz ALTUN <alidenizaltun66@gmail.com>
2026-09-02 11:46:15 +0300

    Mount ToastContainer (Faz 6)
    react-toastify kuruluydu ve NotificationAlert.tsx'in toast.* cagrilari
    zaten kullaniliyordu ama hicbir yerde <ToastContainer/> render edilmiyordu
    - bu cagrilar suana kadar hicbir sey gostermiyordu. Toast bundan boyle
    her iki panelin standart bildirim deseni.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { ToastContainer } from "react-toastify";
 import Router from "@/app/router";
 
 const App = () => {
   return (
+    <>
       <Router />
+      <ToastContainer position="top-right" autoClose={3000} newestOnTop closeOnClick pauseOnHover />
+    </>
   );
 };
 export default App;
